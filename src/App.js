b650227910dd709de9cefd455dd6d916fcc8c563
LucasPJohnson <100321338+LucasPJohnson@users.noreply.github.com>
2022-04-27 16:07:53 -0500

    started on API functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+      loading: true,
+      spell: null,
+    };
+
+    async componentDidMount() {
+        const url = "https://www.dnd5eapi.co/api/spells";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({spell: data.results[0], loading: false });
+    }
+    render() {
+        return (
+            <div className = "tc">
+                {this.state.loading || !this.state.spell ? (
+                  <div>loading...</div>
+                  ) : (
+                  <div>
+                    <div>{this.state.spell.index}</div>
+                    <div>{this.state.spell.name}</div>
+                    <div>{this.state.spell.url}</div>
+                  </div>
+                  )}
+            </div>
+        );
+    }
 }
 
 export default App;
